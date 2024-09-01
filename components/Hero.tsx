b016153189d6f2fbd8afd3b6e54e1c87f348812e
Erick Sandhu <essandhu22@gmail.com>
2024-09-01 16:41:57 -0500

    update hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className="flex justify-center relative mw-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-sm text-center text-blue-100 max-w-120">
-            Modern and Responsive Web Development
+            Modern and Responsive Application Development
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
